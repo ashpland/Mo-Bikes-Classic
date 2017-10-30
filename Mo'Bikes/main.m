85b1cd3474467dbf9442da00d8f8741e164dfb97
Andrew Shopland <hi@ashp.land>
2017-10-30 13:18:12 -0700

    Initial Commit

---
+++ Mo'Bikes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Mo'Bikes
+//
+//  Created by Andrew on 2017-10-30.
+//  Copyright © 2017 hearthedge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
