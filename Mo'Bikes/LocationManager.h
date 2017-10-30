a9573393e0d1d0d6d9a897a191d4a5b300456f48
Sanjay Shah <Sanjay Shah>
2017-10-30 13:43:53 -0700

    added location manager

---
+++ Mo'Bikes/LocationManager.h
@@ -0,0 +1,16 @@
+//
+//  LocationManager.h
+//  Mo'Bikes
+//
+//  Created by Sanjay Shah on 2017-10-30.
+//  Copyright © 2017 hearthedge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface LocationManager : NSObject<CLLocationManagerDelegate>
+
+@property (nonatomic, retain) CLLocationManager *locationManager;
+
+@end
---
+++ Mo'Bikes/LocationManager.m
@@ -0,0 +1,48 @@
+//
+//  LocationManager.m
+//  Mo'Bikes
+//
+//  Created by Sanjay Shah on 2017-10-30.
+//  Copyright © 2017 hearthedge. All rights reserved.
+//
+
+#import "LocationManager.h"
+
+@implementation LocationManager
+
+-(void) getLocation {
+    
+    if (self.locationManager == nil)
+    {
+        self.locationManager = [[CLLocationManager alloc] init];
+        self.locationManager.desiredAccuracy =
+        kCLLocationAccuracyNearestTenMeters;
+        self.locationManager.delegate = self;
+    }
+    [self.locationManager startUpdatingLocation];
+    
+    CLLocation *curPos = self.locationManager.location;
+    NSString *latitude = [[NSNumber numberWithDouble:curPos.coordinate.latitude] stringValue];
+    NSString *longitude = [[NSNumber numberWithDouble:curPos.coordinate.longitude] stringValue];
+  
+     NSLog(@"Lat: %@", latitude);
+     NSLog(@"Long: %@", longitude);
+    
+    
+}
+
+
+
+- (void) locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
+{
+    NSLog(@"%@", @"Core location has a position.");
+}
+
+
+- (void) locationManager:(CLLocationManager *)manager
+        didFailWithError:(NSError *)error
+{
+    NSLog(@"%@", @"Core location can't get a fix.");
+}
+
+@end
